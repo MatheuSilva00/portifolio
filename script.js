8a5d87e5aa7b69492acd1b49cb66e2de41cbc055
MatheuSilva00 <silvamatheu00@gmail.com>
2024-09-02 13:12:11 -0300

    Atualização no js e css

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-// Função para mostrar a seção correspondente ao clique do usuário
-function showSection(sectionId) {
-    // Esconde todas as seções
-    var sections = document.querySelectorAll('section');
-    sections.forEach(function(section) {
-        section.style.display = 'none';
-    });
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
 
-    // Exibe a seção selecionada
-    var selectedSection = document.getElementById(sectionId);
-    selectedSection.style.display = 'block';
+    // Ocultar todas as abas
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remover a classe "active" de todos os links de abas
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Mostrar a aba atual e adicionar uma classe "active" ao link da aba
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
