b9a1cfbbcfc980ceff8972a9b8e6f4aa25b0f3d8
MatheuSilva00 <silvamatheu00@gmail.com>
2024-09-02 14:30:28 -0300

    Edit para os requisitos do trabalho

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
+// Função para abrir uma aba e ocultar outras
 function openTab(evt, tabName) {
-    // Ocultar todas as abas internas
+    // Ocultar todas as seções de conteúdo
     var tabcontent = document.getElementsByClassName("tabcontent");
     for (var i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Remover a classe "active" de todas as abas internas
+    // Remover a classe "active" de todos os links de abas
     var tablinks = document.getElementsByClassName("tablink");
     for (var i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Mostrar a aba atual e adicionar uma classe "active" ao link da aba
-    if (tabName !== 'external') {
-        document.getElementById(tabName).style.display = "block";
-        evt.currentTarget.className += " active";
-    }
+    // Mostrar a aba atual e adicionar a classe "active" ao link da aba
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 
 // Adiciona a classe "active" e exibe a primeira aba ao carregar a página
