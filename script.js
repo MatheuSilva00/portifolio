209447aea036eb19f9c9cee7d968ca446cf84bfe
MatheuSilva00 <silvamatheu00@gmail.com>
2024-09-02 14:12:01 -0300

    correcao funcionamento js das abas

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 function openTab(evt, tabName) {
-    // Ocultar todas as abas
+    // Ocultar todas as abas internas
     var tabcontent = document.getElementsByClassName("tabcontent");
     for (var i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Remover a classe "active" de todas as abas
+    // Remover a classe "active" de todas as abas internas
     var tablinks = document.getElementsByClassName("tablink");
     for (var i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
     // Mostrar a aba atual e adicionar uma classe "active" ao link da aba
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    if (tabName !== 'external') {
+        document.getElementById(tabName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
 }
 
 // Adiciona a classe "active" e exibe a primeira aba ao carregar a página
